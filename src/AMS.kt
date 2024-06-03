612d0f0aa71d0d3fb2e00be61bad621d2f5509be
Gilbert <gilberto.ramirez.suazo@gmail.com>
2024-06-03 17:49:33 -0400

    Functions

--- src/AMS.kt
+++ src/AMS.kt
@@ -1,9 +1,18 @@
 import java.util.*
 
-fun main(args: Array<String>){
+fun main(){
     helloWorld()
     dayOfWeek()
-    getFortuneCookie()
+   // getBirthday()
+    println( fitMoreFish(10.0, listOf(3,3,3)))
+    println(fitMoreFish(8.0, listOf(2,2,2), hasDecorations = false))
+    println(fitMoreFish(9.0, listOf(1,1,3), 3))
+    println( fitMoreFish(10.0, listOf(), 7, true))
+
+}
+
+fun fitMoreFish(tankSize: Double, currentFish: List<Int>, fishSize: Int = 2, hasDecorations: Boolean = true): Boolean {
+    return (tankSize * if (hasDecorations) 0.8 else 1.0) >= (currentFish.sum() + fishSize)
 }
 
 fun helloWorld(){
@@ -26,13 +35,24 @@ fun dayOfWeek(){
     }
 }
 
-fun getFortuneCookie(): String {
-    val fortunes = listOf("You will have a great day!", "Things will go well for you today.", "Enjoy a wonderful day of success.", "Be humble and all will turn out well.", "Today is a good day for exercising restraint.", "Take it easy and enjoy life!", "Treasure your friends because they are your greatest fortune.")
+
+
+fun getBirthday() {
     print("Enter your birthday: ")
-    val birthday = readLine()?.toIntOrNull() ?: 1
+    val birthday= readlnOrNull()?.toIntOrNull() ?: 1
+    println({getFortuneCookie(birthday)})
+}
+
+fun getFortuneCookie(birthday: Int): String {
+    val fortunes = listOf(
+        "You will have a great day!",
+        "Things will go well for you today.",
+        "Enjoy a wonderful day of success.",
+        "Be humble and all will turn out well.",
+        "Today is a good day for exercising restraint.",
+        "Take it easy and enjoy life!",
+        "Treasure your friends because they are your greatest fortune."
+    )
+
     return fortunes[birthday.rem(fortunes.size)]
-    //for (i in 1..10) {
-    //    val fortune = getFortuneCookie()
-    //    println("\nYour fortune is: $fortune")
-    //    if (fortune.contains("Take it easy")) break
 }
