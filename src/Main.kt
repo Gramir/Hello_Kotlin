25f0e0fcc2dfffd9ed0a2282c630eaf6c213abf0
Gilbert <gilberto.ramirez.suazo@gmail.com>
2024-06-09 16:37:53 -0400

    Beyond the basics

--- src/Main.kt
+++
@@ -1,9 +0,0 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-fun main() {
-
-    fun helloWorld(){
-        println("Hello World")
-    }
-    helloWorld()
-}
